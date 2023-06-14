54818a46a07e17b45e3658a7b1ebc4b5054cff1c
Vladyslavua <vladgershman@gmail.com>
2023-06-14 20:51:01 +0200

    update: Done with Hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import Button from "../CustomButton/CustomButton";
+import CustomButton from "../CustomButton/CustomButton";
+import Image from "next/image";
 
 const Hero = () => {
 
@@ -23,13 +24,19 @@ const Hero = () => {
                     Streamline your car rental experience
                     with our effortless booking process
                 </p>
-                <Button
+                <CustomButton
                     title='Explore Cars'
                     containerStyles='bg-primary-blue text-white
                               rounded-full mt-10'
                     handleClick={handleScroll}
 
                 />
+            </div>
+            <div className='hero__image-container'>
+                <div className='hero__image'>
+                    <Image src={'/hero.png'} alt={'hero'} fill className='object-contain' />
+                </div>
+                    <div className={'hero__image-overlay'}/>
             </div>
         </div>
     );
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from "@/components/Hero/Hero";
 import Button from "@/components/CustomButton/CustomButton";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 export {
     Hero,
-    Button
+    Button,
+    Navbar,
+    Footer
 }
