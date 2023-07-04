6d27907c1700314497604a9276ab0791c99f98cf
Vladyslavua <vladgershman@gmail.com>
2023-07-04 18:53:28 +0200

    update:added imports

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,19 @@ import Hero from "@/components/Hero/Hero";
 import Button from "@/components/CustomButton/CustomButton";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import CarCard from "@/components/CarCard/CarCard";
+import ShowMore from "@/components/ShowMore/ShowMore";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import CustomFilter from "@/components/CustomFilter/CustomFilter";
+
 
 export {
     Hero,
     Button,
     Navbar,
-    Footer
+    Footer,
+    CarCard,
+    ShowMore,
+    SearchBar,
+    CustomFilter
 }
